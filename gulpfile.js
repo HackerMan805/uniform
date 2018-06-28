ca7f5fd3b698d3fd507898454279ff3bb915b586
Michael Li <michael@scholantis.com>
2018-06-27 17:34:13 -0700

    add svg icons to src/icons/ use svgstore to add inline svg use handlebarsjs to generate icon sprite move index.html to src/

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,26 @@ const gulp = require('gulp');
 const open = require('open');
 const sass = require('gulp-sass');
 const webpack = require('webpack-stream');
+const svgstore = require('gulp-svgstore');
+const inject = require('gulp-inject');
+const glob = require('glob');
+const path = require('path');
+const handlebars = require('gulp-compile-handlebars');
 
 const app = {
     sassRoot: './src/sass/',
     js: './src/js/app.js',
     sass: './src/sass/**/*.scss',
-    dest: './libs'
+    dest: './libs',
+    icons: './src/icons/*.svg',
+    html: './src/*.html'
 };
 const demoApp = {
     sassRoot: ['./docs/sass/', './src/sass'],
     sass: './docs/sass/**/*.scss',
     dest: './docs/css',
-    jsDest: './docs/js'
+    jsDest: './docs/js',
+    destFolder: './docs/'
 };
 
 function compileSass (app) {
@@ -24,6 +32,36 @@ function compileSass (app) {
             }).on('error', sass.logError))
             .pipe(gulp.dest(app.dest));
     };
+}
+
+function compileHtml(iconSprite) {
+    let svgs = gulp.src(app.icons)
+    .pipe(svgstore({inlineSvg: true}));
+
+    function fileContents (filePath, file) {
+        return file.contents.toString();
+    }
+
+    let templateData = {          
+        iconSprite
+    },        
+    options = {
+        helpers : {
+            list : function(context, options) {
+                var ret = "";
+                for(var i=0, j=context.length; i<j; i++) {
+                    ret = ret + options.fn(context[i]);
+                }
+                
+                return ret;
+            }
+        }
+    }                
+
+    return gulp.src(app.html)
+        .pipe(inject(svgs, { transform: fileContents }))
+        .pipe(handlebars(templateData, options))
+        .pipe(gulp.dest(demoApp.destFolder));
 }
 
 gulp.task('sass:watch', function() {
@@ -43,10 +81,26 @@ gulp.task('demo:js', () => {
         .pipe(gulp.dest(demoApp.jsDest));
 });
 
+gulp.task('html', (done) => {
+    glob(app.icons, function (err, icons) {
+        icons = icons.map(function(icon){
+            return { icon: path.basename(icon, '.svg') };
+        }); 
+
+        if (err) {
+            console.log(err);
+            return;
+        }   
+
+        compileHtml(icons); 
+    });
+    done();
+});
+
 gulp.task('start', (done) => {
     open('./docs/index.html');
     done();
 });
-gulp.task('build', gulp.parallel('sass', 'demo:sass', 'js', 'demo:js'));
+gulp.task('build', gulp.parallel('sass', 'demo:sass', 'html', 'js', 'demo:js'));
 gulp.task('default', gulp.series('build'));
 
